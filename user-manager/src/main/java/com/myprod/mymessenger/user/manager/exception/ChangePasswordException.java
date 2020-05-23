08a97a570ff6a31b446f2a1766ff3819b0a313e5
Vasiliy_Sennikov <vasyansennicov@mail.ru>
2020-05-24 01:46:15 +0700

    MYM-38 Add stub for changePassword like to exception in UserService Added exception-class in the package. Implemented Exception logic

---
+++ user-manager/src/main/java/com/myprod/mymessenger/user/manager/exception/ChangePasswordException.java
@@ -0,0 +1,7 @@
+package com.myprod.mymessenger.user.manager.exception;
+
+public class ChangePasswordException extends Exception {
+  public ChangePasswordException (String message) throws Exception{
+    super(message);
+  }
+}
