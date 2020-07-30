cb3781bf53b24ea222279a7b45d0dfec7ae4dc80
Vasiliy_Sennikov <vasyansennicov@mail.ru>
2020-07-30 23:27:28 +0700

    MYM-43 Add validator for unique email Fixed comments

--- user-manager/backend/src/main/java/com/myprod/mymessenger/user/manager/service/handler/ControllerExceptionHandler.java
+++ user-manager/backend/src/main/java/com/myprod/mymessenger/user/manager/service/handler/ControllerExceptionHandler.java
@@ -2,8 +2,6 @@ package com.myprod.mymessenger.user.manager.service.handler;
 
 import com.myprod.mymessenger.user.manager.model.view.ErrorView;
 import javax.servlet.http.HttpServletRequest;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,11 +9,10 @@ import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-@Slf4j
 public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
   @ExceptionHandler({ResponseStatusException.class})
   public ResponseEntity<ErrorView> errorHandler(HttpServletRequest req, ResponseStatusException e) {
-    ErrorView errorView = new ErrorView(e.getMessage(), 400, e.getHeaders());
-    return new ResponseEntity<>(errorView, HttpStatus.BAD_REQUEST);
+    ErrorView errorView = new ErrorView(e.getMessage(), e.getStatus().value(), e.getHeaders());
+    return new ResponseEntity<>(errorView, e.getStatus());
   }
 }
