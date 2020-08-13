1cc82b82c941ccb95faa0d27ec84d5aaa3258d4c
kelbek <dev_kelbek@mail.ru>
2020-08-14 01:07:55 +0300

    [MYM-60] add signup page (layout)

---
+++ user-manager/frontend/pages/_app.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import '../styles/theme.scss'
+import 'react-phone-input-2/lib/style.css'
+import 'typeface-roboto'
+import 'normalize.css'
+
+export default ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+}
---
+++ user-manager/frontend/pages/index.tsx
@@ -0,0 +1,12 @@
+// import {Container, Row, Col, Button} from 'react-bootstrap';
+import Button from '@material-ui/core/Button'; 
+
+function Landing() {
+  return (
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+  );
+}
+
+export default Landing;
